dc91d39fcdc2f3683b974e717f416dfee26ce8b6
lucyCummins <54380389+lucyCummins@users.noreply.github.com>
2019-08-21 16:17:12 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int hello1Function(string name1);
+int hello2Function(string name2);
+int main()
+{
+	string name1="Lucy Cummins";
+	hello1Function(name1);
+	string name2="Jack Loague";
+	hello2Function(name2);
+	return 0;
+}
